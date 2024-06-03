b2413cc51ee168843946a02e82f8e286dd46eb8f
ayush1108g <105841420+ayush1108g@users.noreply.github.com>
2024-06-03 16:48:23 +0530

    update in next config js

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -10,6 +10,27 @@ const nextConfig = {
       { hostname: "blog-web-4yaz.onrender.com/api/fileupload" },
     ],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "https://blog-web-4yaz.onrender.com/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
