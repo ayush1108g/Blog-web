e21848911a34e66d4ebc51f1d2966b72e7fcce8f
ayush1108g <105841420+ayush1108g@users.noreply.github.com>
2024-06-03 17:39:57 +0530

    fix date issue

--- client/src/app/blogs/[id]/page.tsx
+++ client/src/app/blogs/[id]/page.tsx
@@ -1,19 +1,16 @@
 "use client";
 import React, { useState, useEffect, useRef } from 'react';
-import { usePathname,useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import Comments from '@/components/Comments';
 import { useLogin } from '@/context/LoginContext';
 import { useAlert } from '@/context/AlertContext';
-import {API } from '@/utils/api';
+import { API } from '@/utils/api';
 import Image from 'next/image';
 import "suneditor/dist/css/suneditor.min.css";
 
-function extractDateTimeFromTimestamp(timestamp:number) {
-  // Convert timestamp to milliseconds (if not already in milliseconds)
-  if (timestamp.toString().length < 13) {
-      timestamp *= 1000;
-  }
-
+function extractDateTimeFromTimestamp(timestamp: string) {
+ 
+  console.log(timestamp);
   // Create a new Date object
   const date = new Date(timestamp);
 
@@ -29,10 +26,11 @@ function extractDateTimeFromTimestamp(timestamp:number) {
 
   // Return an object containing date and time components
   return {
-      date: `${day}/${month}/${year}`,
-      time: `${hours}:${minutes}:${seconds}`
+    date: `${day}/${month}/${year}`,
+    time: `${hours}:${minutes}:${seconds}`
   };
 }
+
 interface Blog {
   _id: string;
   title: string;
@@ -49,10 +47,10 @@ interface Blog {
 
 interface Comment {
   userId: {
-      name: string;
-      photo: string;
-      _id: string;
-      email: string;
+    name: string;
+    photo: string;
+    _id: string;
+    email: string;
   }
   date: string;
   comment: string;
@@ -61,6 +59,7 @@ interface Comment {
   like: [string];
   dislike: [string];
 }
+
 const Page = () => {
   const pathname = usePathname();
   const router = useRouter();
@@ -79,47 +78,46 @@ const Page = () => {
     fetchComments();
   }, [id]);
 
-
   const fetchBlog = async () => {
     try {
-      const blogs = await API.get('/api/v1/blogs/blog/'+id);
+      const blogs = await API.get('/api/v1/blogs/blog/' + id);
       if (!blogs) {
         throw new Error('No blogs found with this id');
       }
       console.log(blogs);
-      
+
       const selectedBlog = blogs.data.blog;
 
       if (!selectedBlog) {
         throw new Error(`Blog with ID ${id} not found.`);
       }
-      // selectedBlog.data = replaceImagesWithDivs(selectedBlog.data);
       setBlog(selectedBlog);
-    } catch (err:any) {
+    } catch (err: any) {
       setError(err.message);
     } finally {
       setLoading(false);
     }
   };
+
   const fetchComments = async () => {
-      try{
-          const comments = await API.get('/api/v1/comments/'+id);
-          if (!comments) {
-              throw new Error('No comments found for this blog');
-          }
-          console.log(comments);
-          const comm = comments.data.comments;
-          comm.sort((a:Comment, b:Comment) => new Date(b.date).getTime() - new Date(a.date).getTime());
-          setComments(comments.data.comments);
-      }catch(err){
-         console.log(err);
+    try {
+      const comments = await API.get('/api/v1/comments/' + id);
+      if (!comments) {
+        throw new Error('No comments found for this blog');
       }
+      console.log(comments);
+      const comm = comments.data.comments;
+      comm.sort((a: Comment, b: Comment) => new Date(b.date).getTime() - new Date(a.date).getTime());
+      setComments(comments.data.comments);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   useEffect(() => {
     if (blockRef.current) {
       const blockHeight = blockRef.current.offsetHeight;
-      setMarkersCount(Math.ceil(blockHeight /( window.innerHeight*1.5))); 
+      setMarkersCount(Math.ceil(blockHeight / (window.innerHeight * 1.5)));
     }
   }, [blog]);
 
@@ -135,33 +133,30 @@ const Page = () => {
     router.push(`/blogs/${id}/edit`);
   };
 
- 
-
-
   return (
     <div className="relative bg-grey-200 pt-20">
       <div className="absolute top-100 left-0 w-full z-0 pointer-events-none">
         {[...Array(markersCount)].map((_, index) => (
-          <p key={index} className="text-gray-200 text-6xl font-bold absolute transform -rotate-45" style={{ top: `${ index * 150+50}vh`, left:'35%',  }}>
+          <p key={index} className="text-gray-200 text-6xl font-bold absolute transform -rotate-45" style={{ top: `${index * 150 + 50}vh`, left: '35%' }}>
             Blogging Site
           </p>
         ))}
       </div>
 
-        {blog?.userId._id === LoginCtx?.user?._id &&  
-         <button onClick={handleEditClick} className="absolute top-0 right-0 mt-4 mr-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+      {blog?.userId._id === LoginCtx?.user?._id &&
+        <button onClick={handleEditClick} className="absolute top-0 right-0 mt-4 mr-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
           Edit
         </button>}
       {/* Page content */}
-      <div className="relative z-10 bg-grey-200  border-2 border-grey-100 m-2 p-5" ref={blockRef}>
+      <div className="relative z-10 bg-grey-200 border-2 border-grey-100 m-2 p-5" ref={blockRef}>
         <h1 className="text-center font-bold text-3xl capitalize">
           {blog ? blog.title : 'Blog not found'}
         </h1>
-        <p className="absolute top-12 right-10 mt-4 mr-4  text-black py-2 px-4 rounded hover:bg-pink-200"        >
-          {blog ?'~ '+ blog.userId.name : ''}
-        <p className="absolute top-18 mr-4 text-black py-2 px-4 rounded hover:bg-pink-200"        >
-          {blog ? extractDateTimeFromTimestamp(parseInt(blog.date)).date : ''}
-        </p>
+        <p className="absolute top-12 right-10 mt-4 mr-4 text-black py-2 px-4 rounded hover:bg-pink-200">
+          {blog ? '~ ' + blog.userId.name : ''}
+          <p className="absolute top-18 mr-4 text-black py-2 px-4 rounded hover:bg-pink-200">
+            {blog ? extractDateTimeFromTimestamp(blog.date).date : ''}
+          </p>
         </p>
         {blog?.coverImage && (
           <div className="flex justify-center mb-6">
@@ -176,16 +171,16 @@ const Page = () => {
             </div>
           </div>
         )}
-     
-       <div className="sun-editor-editable">
-        <div style={{ maxWidth: '100%', padding: '30px', alignItems: 'center' }}>
-          <div dangerouslySetInnerHTML={{ __html: blog? blog.data : '' }} />
-        </div>
+
+        <div className="sun-editor-editable">
+          <div style={{ maxWidth: '100%', padding: '30px', alignItems: 'center' }}>
+            <div dangerouslySetInnerHTML={{ __html: blog ? blog.data : '' }} />
+          </div>
         </div>
       </div>
 
       {/* Comments section */}
-      <Comments blogId={id} comments={comments} setComments={setComments}/>
+      <Comments blogId={id} comments={comments} setComments={setComments} />
     </div>
   );
 };
