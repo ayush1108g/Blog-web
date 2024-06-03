da0a7102461e9eda870c82e15c28969f4d0c9709
ayush1108g <105841420+ayush1108g@users.noreply.github.com>
2024-06-03 16:51:55 +0530

    next.config.js update

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,19 +2,18 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { hostname: "picsum.photos" },
-      { hostname: "cdn.example" },
-      { hostname: "drive.google.com" },
-      { hostname: "lh3.googleusercontent.com" },
-      { hostname: "blog-web-4yaz.onrender.com" },
-      { hostname: "blog-web-4yaz.onrender.com/api/fileupload" },
+      { protocol: "https", hostname: "picsum.photos" },
+      { protocol: "https", hostname: "cdn.example" },
+      { protocol: "https", hostname: "drive.google.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "blog-web-4yaz.onrender.com" },
     ],
   },
   async headers() {
     return [
       {
         // matching all API routes
-        source: "https://blog-web-4yaz.onrender.com/api/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
